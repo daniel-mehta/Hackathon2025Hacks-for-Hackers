589b770d3ab093f2f87f0d7ca05b44ec087eb723
daniel-mehta <daniel.k.mehta@gmail.com>
2025-01-03 16:56:35 -0500

    all-players works

--- PredicitionAppFPL/App/main.py
+++ PredicitionAppFPL/App/main.py
@@ -17,6 +17,11 @@ def fetch_top_10_players():
     top_10 = get_top_10_players(sorted_players_df)
     return top_10.to_dict(orient='records')
 
+@app.get("/all-players")
+def fetch_sorted_players():
+    clean_df = sorted_players_df.replace([float("inf"), float("-inf")], None).fillna(0)
+    return clean_df.to_dict(orient='records')
+
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8000)
