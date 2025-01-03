03bdf73be87f20073fa21be31a0eaef706cd5ad5
daniel-mehta <daniel.k.mehta@gmail.com>
2025-01-03 17:23:41 -0500

    Top 5 per position working

--- PredicitionAppFPL/App/main.py
+++ PredicitionAppFPL/App/main.py
@@ -3,7 +3,8 @@ from xp_fpl import (
     get_fpl_data,
     calculate_expected_points,
     get_top_10_players,
-    sorted_players_df
+    sorted_players_df,
+    get_top_players_by_position
 )
 
 app = FastAPI()
@@ -21,6 +22,15 @@ def fetch_top_10_players():
 def fetch_sorted_players():
     clean_df = sorted_players_df.replace([float("inf"), float("-inf")], None).fillna(0)
     return clean_df.to_dict(orient='records')
+
+@app.get("/best-per-position")
+def fetch_get_top_players_by_position():
+    positions = ['GK', 'DEF', 'MID', 'FWD']
+    results = {}
+    for pos in positions:
+        top_players = get_top_players_by_position(sorted_players_df, pos)
+        results[pos] = top_players.to_dict(orient='records')
+    return results
 
 if __name__ == "__main__":
     import uvicorn
